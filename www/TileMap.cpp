791cc9b04f20b2923351c83c5c6cf16551340d26
dja89 <drod89@gmail.com>
2019-03-19 18:55:24 +0100

    Adjust jump speed

--- www/TileMap.cpp
+++ www/TileMap.cpp
@@ -270,7 +270,7 @@ bool TileMap::collisionMoveDown(const glm::ivec2 &pos, const glm::ivec2 &size, i
 	{
 		if(map[y*mapSize.x+x] != 0)
 		{
-			if(*posY - tileSize * y + size.y <= 4)
+			if(*posY - tileSize * y + size.y <= 6)
 			{
 				*posY = tileSize * y - size.y;
 				return true;
@@ -292,7 +292,7 @@ bool TileMap::collisionMoveUp(const glm::ivec2 &pos, const glm::ivec2 &size, int
 	{
 		if(map[y*mapSize.x+x] != 0)
 		{
-			if(*posY - tileSize * y >= 12)
+			if(*posY - tileSize * y >= 10)
 			{
 				*posY = tileSize * (y + 1);
 			}
